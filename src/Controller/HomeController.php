7f4f2f919cad8223749602728dc6e94e706281f8
Ulisses <ualbardeiro@gmail.com>
2026-01-19 13:48:56 -0300

    Envia dados para a view

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
+    public function __construct(
+        private CategoryRepository $categoryRepository,
+        private PostRepository $postRepository,
+        ) {}
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
             'title' => 'Inicio do blog',
+            'categories' => $this->categoryRepository->findAll(),
+            'posts' => $this->postRepository->findAll(),
         ]);
     }
 }
